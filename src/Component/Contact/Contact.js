89a1d550b40e2973cab0bb1402f73a9699747db8
Adrianna Valois <adriannavalois@Adriannas-MacBook.local>
2019-06-14 17:49:26 -0700

    contact page

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./Contact.css";
+import GitHub from "./GitHub-Mark-32px.png";
+import LI from "./LI-In-Bug.png";
 
 const Contact = () => {
-  return <div>contact info</div>;
+  return (
+    <div className="contact-main">
+      Contact Info
+      <ul>
+        <a
+          href="www.linkedin.com/in/adrianna-valois"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-img"
+        >
+          LinkedIn <img src={LI} alt="linkedin Logo" />
+        </a>
+
+        <a
+          href="https://github.com/vkamelie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+          <img src={GitHub} alt="GitHub Logo" />
+        </a>
+        <li>valois.adrianna@gmail.com</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Contact;
--- src/Component/Projects/Projects.js
+++ src/Component/Projects/Projects.js
@@ -1,26 +1,22 @@
 import React from "react";
 import Transplant from "./TransplantProject.png";
 import GitHub from "./GitHub-Mark-Light-32px.png";
+import "./Projects.css";
 
 const Projects = () => {
   return (
-    <div>
+    <div className="projects-main">
       Projects
       <div>
-        <header>Transplant Community Support</header>
-        <p>
+        <h1>Transplant Community Support</h1>
+        <h2>
           A personal project created durning my time at DevMoutian. It is fourm
           and chat app specifly for the transplant community.
-        </p>
+        </h2>
         <img src={Transplant} alt="Transplant Support Project" />
-
         <a href="https://github.com/vkamelie/Support">
           <img src={GitHub} alt="GitHub logo" />
         </a>
-      </div>
-      <div>
-        <header />
-        <p />
       </div>
     </div>
   );
