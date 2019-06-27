062d9228b0ad219ffc4965523ed24faef5e6bd79
Adrianna Valois <adriannavalois@Adriannas-MacBook.local>
2019-06-27 15:58:44 -0700

    Updates

---
+++ precache-manifest.5f0ab14bdcee507e13e3a8e89c939cf7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c6c0c770e9c7521c8bf9f51a5f803b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "97fe069fd5f49fc2a4dd",
+    "url": "/Portfolio/static/css/main.ee148f3e.chunk.css"
+  },
+  {
+    "revision": "b618c23bcab70ef21e15",
+    "url": "/Portfolio/static/js/2.d3b20218.chunk.js"
+  },
+  {
+    "revision": "97fe069fd5f49fc2a4dd",
+    "url": "/Portfolio/static/js/main.50664bdb.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "42ffc07ffccc888dc2f5627d882a66be",
+    "url": "/Portfolio/static/media/DD1.42ffc07f.png"
+  },
+  {
+    "revision": "1888c3605efe7c34c6e82bd70c136383",
+    "url": "/Portfolio/static/media/DD2.1888c360.png"
+  },
+  {
+    "revision": "088618195129f04430c4b6f156d7036c",
+    "url": "/Portfolio/static/media/DD3.08861819.png"
+  },
+  {
+    "revision": "f957c2a3f6bb8ae2fe6304784bbdba30",
+    "url": "/Portfolio/static/media/DD4.f957c2a3.png"
+  },
+  {
+    "revision": "37196a46b534be9ef54cdb3a62c5a88e",
+    "url": "/Portfolio/static/media/Tp1.37196a46.png"
+  },
+  {
+    "revision": "256b8f7a9a2a5ebb092a655e7d34001b",
+    "url": "/Portfolio/static/media/Tp2.256b8f7a.png"
+  },
+  {
+    "revision": "ade09a50d537e6fc6904f4c39c8412e2",
+    "url": "/Portfolio/static/media/Tp3.ade09a50.png"
+  },
+  {
+    "revision": "f46ecd275a1e50a3ef28755b6407c793",
+    "url": "/Portfolio/static/media/Tp4.f46ecd27.png"
+  }
+]);
